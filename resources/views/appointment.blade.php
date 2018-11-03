a034075e5c8c906dc56cc7dfbcfa289362f0727a
Elijah <ejoebanks@gmail.com>
2018-11-02 19:14:38 -0500

    Styling changes

--- resources/views/appointment.blade.php
+++ resources/views/appointment.blade.php
@@ -1,13 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-
-
 <div class="container">
 	<div class="row">
 <div class="col-md-6">
-                    <div class="well-block">
-                        <div class="well-title">
+  <div class="well-block">
+              <div class="well-title">
                             <h2>Request an Appointment</h2>
                         </div>
                         <form>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,102 +1,110 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-<div id ="container">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/bstrap.css') }}">
-
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-confirmation/1.0.5/bootstrap-confirmation.min.js"></script>
-
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ ('Horse Braiding Scheduling') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div id="app">
-      <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ ('Horse Braiding Scheduling') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
-                            <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
-
-                        @else
-                        <?php
-                        $id = Auth::user()->id;
-                        if ($id == 1){ ?>
-                          <li><a class="nav-link" href="/home" class="dropdown-item">View Schedule</a></li>
-                        <?php } ?>
-
-                            <li><a class="nav-link" href="/gallery">Gallery</a></li>
-                            <li><a class="nav-link" href="#">Place Order</a></li>
-
-                            <li class="nav-item dropdown">
-
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                  {{ Auth::user()->firstname." ".Auth::user()->lastname }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-
-                                    <a href="{{action('UserController@singleEdit', $id)}}" class="dropdown-item">Update Account</a>
-
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+  <div id ="container">
+    <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+      <link rel="stylesheet" href="{{ asset('css/bstrap.css') }}">
+      <script src="https://code.jquery.com/jquery-3.2.1.min.js">
+      </script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js">
+      </script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-confirmation/1.0.5/bootstrap-confirmation.min.js">
+      </script>
+      <!-- CSRF Token -->
+      <meta name="csrf-token" content="{{ csrf_token() }}">
+      <title>{{ ('Horse Braiding Scheduling') }}
+      </title>
+      <!-- Scripts -->
+      <script src="{{ asset('js/app.js') }}" defer>
+      </script>
+      <!-- Fonts -->
+      <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+      <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+      <!-- Styles -->
+      <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+      <div id="app">
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+          <div class="container">
+            <a class="navbar-brand" href="{{ url('/') }}">
+              {{ ('Horse Braiding Scheduling') }}
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon">
+              </span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <!-- Left Side Of Navbar -->
+              <ul class="navbar-nav mr-auto">
+              </ul>
+              <!-- Right Side Of Navbar -->
+              <ul class="navbar-nav ml-auto">
+                <!-- Authentication Links -->
+                @guest
+                <li>
+                  <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}
+                  </a>
+                </li>
+                <li>
+                  <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}
+                  </a>
+                </li>
+                @else
+                <?php
+$id = Auth::user()->id;
+          if ($id == 1) {
+            ?>
+                <li>
+                  <a class="nav-link" href="/home" class="dropdown-item">View Schedule
+                  </a>
+                  <?php
+              } ?>
+                <li>
+                  <a class="nav-link" href="/gallery">Gallery
+                  </a>
+                </li>
+                <li>
+                  <a class="nav-link" href="#">Place Order
+                  </a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    {{ Auth::user()->firstname." ".Auth::user()->lastname }}
+                    <span class="caret">
+                    </span>
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a href="{{action('UserController@singleEdit', $id)}}" class="dropdown-item">Update Account
+                    </a>
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                       onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                      {{ __('Logout') }}
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                      @csrf
+                    </form>
+                  </div>
+                </li>
+                @endguest
+              </ul>
             </div>
+          </div>
         </nav>
-
         <main class="py-4">
-            @yield('content')
+          @yield('content')
         </main>
-    </div>
+      </div>
+      </div>
+    </body>
+  <div id="footer">
+    © 2018
+    <strong>Elijah Banks, Joshua Razalan, Joseph Kovack, Adam Deisz
+    </strong>
   </div>
-</body>
-<div id="footer">
-        © 2018 <strong>Elijah Banks, Joshua Razalan, Joseph Kovack, Adam Deisz</strong>
-</div>
-</div>
+  </div>
 </html>
