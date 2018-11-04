2f9c3cef8e7eb7094f46eaff7100c3acfa6ba0ae
Elijah <ejoebanks@gmail.com>
2018-11-04 15:09:46 -0600

    Added view for when an order has been submitted

---
+++ resources/views/layouts/orderplaced.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+<div class="row">
+  <div class="col-md-3"></div>
+  <div class="col-md-6">
+    <div class="centerBlock">
+      <h1 class="display-1">Order Placed</h1>
+    </div>
+  </div>
+  <div class="col-md-3"></div>
+</div>
+
+
+@endsection
