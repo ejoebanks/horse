86b658c59804faae23dbda3325310c24434be925
Elijah <ejoebanks@gmail.com>
2018-11-04 15:07:01 -0600

    Edited scheduling form

--- resources/views/appointment.blade.php
+++ resources/views/appointment.blade.php
@@ -2,78 +2,92 @@
 
 @section('content')
 <div class="container">
-	<div class="row">
-<div class="col-md-6">
-  <div class="well-block">
-              <div class="well-title">
-                            <h2>Request an Appointment</h2>
-                        </div>
-                        <form>
-                            <!-- Form start -->
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="control-label" for="name">Horse Name</label>
-                                        <input id="name" name="name" type="text" placeholder="Name" class="form-control input-md">
-                                    </div>
-                                </div>
-                                <!-- Text input-->
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="control-label" for="location">Location</label>
-                                        @include('functions.locations')
-                                    </div>
-                                </div>
-																<div class="col-md-6">
-																		<div class="form-group">
-																				<label class="control-label" for="location">Buildings</label>
-																				@include('functions.buildings')
-																		</div>
-																</div>
+	<form method="post" action="{{ action('OrderController@scheduleAppt') }}">
+		<div class="col-md-6">
+			<h2>Request an Appointment</h2>
+			<div class="row">
+				<div class="col-md-6">
+					<div class="form-group">
+						<input type="hidden" value="{{csrf_token()}}" name="_token" />
+						<label class="control-label" for="name">Horse Name</label>
+						<input id="horsename" name="horsename" type="text" placeholder="Name" class="form-control input-md">
+					</div>
+				</div>
 
-                                <!-- Text input-->
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="control-label" for="date">Date</label>
-                                        <input id="date" name="date" type="text" placeholder="Date" class="form-control input-md">
-                                    </div>
-                                </div>
-                                <!-- Select Basic -->
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="control-label" for="time">Time</label>
-                                        <select id="time" name="time" class="form-control">
-                                            <option value="8:00 to 9:00">8:00 to 9:00</option>
-                                            <option value="9:00 to 10:00">9:00 to 10:00</option>
-                                            <option value="10:00 to 1:00">10:00 to 1:00</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <!-- Select Basic -->
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="control-label" for="appointmentfor">Appointment For</label>
-                                        @include('functions.services')
-                                    </div>
-                                </div>
+				<div class="col-md-6">
+					<div class="form-group">
+						<input type="hidden" value="{{csrf_token()}}" name="_token" />
+						<label class="control-label" for="stablenumber">Stable Number:</label>
+						<input id="stablenumber" name="stablenumber" type="number" placeholder="" class="form-control input-md">
+					</div>
+				</div>
 
-                                <div class="col-md-12">
-                                    <div class="form-group">
-                                        <label class="control-label" for="appointmentfor">Comments</label>
-                                        <textarea class="form-control" rows="5" id="comment"></textarea>
-                                    </div>
-                                </div>
+				<div class="col-md-6">
+					<div class="form-group">
+						<input type="hidden" value="{{csrf_token()}}" name="_token" />
+						<label class="control-label" for="locationid">Location</label>
+						@include('functions.locations')
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="form-group">
+						<input type="hidden" value="{{csrf_token()}}" name="_token" />
+						<label class="control-label" for="buildingid">Buildings</label>
+						@include('functions.buildings')
+					</div>
+				</div>
 
-                                <!-- Button -->
-                                <div class="col-md-12">
-                                    <div class="form-group">
-                                        <button id="singlebutton" name="singlebutton" class="btn btn-default">Make An Appointment</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-                        <!-- form end -->
-                    </div>
-                </div>	</div>
+				<div class="col-md-6">
+					<div class="form-group">
+						<input type="hidden" value="{{csrf_token()}}" name="_token" />
+						<label class="control-label" for="scheduledtime">Date</label>
+						<input id="scheduledtime" name="scheduledtime" type="text" placeholder="Date" class="form-control input-md">
+					</div>
+				</div>
+
+				<div class="col-md-6">
+					<div class="form-group">
+						<input type="hidden" value="{{csrf_token()}}" name="_token" />
+						<label class="control-label" for="serviceid">Service</label>
+						@include('functions.services')
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="form-group">
+						<input type="hidden" value="{{csrf_token()}}" name="_token" />
+						<label for="employeeid">Employee:</label>
+						<select class="form-control" value="employeeid" name="employeeid">
+							<?php
+													$users = DB::table('users')
+														->where('users.type', '!=', '0')
+														->distinct()
+														->get(); ?>
+							<option value="">None</option>
+
+							<?php foreach ($users as $uid) {
+																?>
+							<option value="<?= $uid->id ?>">
+								<?= $uid->firstname, " ".$uid->lastname ?>
+							</option>
+							<?php
+														} ?>
+						</select>
+					</div>
+				</div>
+
+
+				<div class="col-md-12">
+					<div class="form-group">
+						<input type="hidden" value="{{csrf_token()}}" name="_token" />
+						<label class="control-label" for="comments">Comments</label>
+						<textarea class="form-control" rows="5" id="comments"></textarea>
+					</div>
+				</div>
+
+			</div>
+			<button type="submit" class="btn btn-default">Make Appointment</button>
+		</div>
+</div>
+</form>
 </div>
 @endsection
