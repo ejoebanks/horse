4131ed6e92233dc1ba255d9e422136ebae27458c
Elijah <ejoebanks@gmail.com>
2018-11-05 18:59:27 -0600

    Added calendar functionality

---
+++ resources/views/calendar.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+@section('style')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.css"/>
+@endsection
+@section('content')
+<div class="container">
+   <div class="row">
+     {!! $cal->calendar() !!}
+   </div>
+</div>
+@endsection
+@section('script')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.js"></script>
+{!! $cal->script() !!}
+@endsection
