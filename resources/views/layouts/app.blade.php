ac0b92b5cfeddffcc9366aab274e34e0f4d6bda8
Elijah <ejoebanks@gmail.com>
2018-11-24 18:07:08 -0600

    Navbar changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,15 @@
       <title>{{ ('Horse Braiding Scheduling') }}
       </title>
       <!-- Scripts -->
-
+      <?php
+      if(is_Object(Auth::user())){
+        $id = Auth::user()->type;
+        $requestedAppts = DB::table('orders')
+           ->where('orders.employeeid', Auth::user()->id)
+           ->where('orders.status', 0)
+           ->count();
+      }
+      ?>
       <script src="{{ asset('js/app.js') }}" >
       </script>
 
@@ -25,86 +33,110 @@
     </head>
     <body>
       <div id="app">
-        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <nav class="navbar navbar-icon-top navbar-expand-lg navbar-dark bg-dark">
           <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">
-              {{ ('Horse Braiding Scheduling') }}
-            </a>
+            <a class="navbar-brand" href="/">IHBO</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon">
-              </span>
+              <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <!-- Left Side Of Navbar -->
-              <ul class="navbar-nav mr-auto">
-              </ul>
-              <!-- Right Side Of Navbar -->
-              <ul class="navbar-nav ml-auto">
-                <!-- Authentication Links -->
-                @guest
-                <li>
-                  <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}
-                  </a>
-                </li>
-                <li>
-                  <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}
-                  </a>
-                </li>
-                @else
-                <?php
-                $id = Auth::user()->type;
-          if ($id > 0) {
-              ?>
-              <li>
-                <a class="nav-link" href="/home" class="dropdown-item">Appointments
-                </a>
-              </li>
-              <li>
-                <a class="nav-link" href="/calendar" class="dropdown-item">Schedule
-                </a>
-              </li>
-                  <?php
-          } ?>
-                <li>
-                  <a class="nav-link" href="/gallery">Gallery
-                  </a>
-                </li>
-                <li>
-                  <a class="nav-link" href="/schedule">Place Order
-                  </a>
-                </li>
-                <li class="nav-item dropdown">
-                  <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                    {{ Auth::user()->firstname." ".Auth::user()->lastname }}
-                    <span class="caret">
-                    </span>
-                  </a>
-                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <?php $uid = Auth::user()->id; ?>
-                    <a href="/ordersummary" class="dropdown-item">View Orders</a>
 
-                    <a href="{{action('UserController@singleEdit', $uid)}}" class="dropdown-item">Update Account
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="/">
+                    <i class="fa fa-home"></i>
+                    Home
+                    <span class="sr-only">(current)</span>
                     </a>
-                <?php if ($id > 0) {
-              ?>
-                  <a href="/admin" class="dropdown-item">Admin</a>
-        <?php
-          } ?>
-                    <a class="dropdown-item" href="{{ route('logout') }}"
-                       onclick="event.preventDefault();
-                                document.getElementById('logout-form').submit();">
-                      {{ __('Logout') }}
-                    </a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                      @csrf
-                    </form>
-                  </div>
                 </li>
-                @endguest
+                <?php
+          if (isSet($id) && $id > 0) {
+              ?>
+
+                <li class="nav-item">
+                  <a class="nav-link" href="/home">
+                    <i class="fa fa-envelope-o">
+                      <span class="badge badge-danger"><?php echo intval($requestedAppts); ?></span>
+                    </i>
+                    Appointments
+                  </a>
+                </li>
+
+                <li class="nav-item">
+                  <a class="nav-link" href="/home">
+                    <i class="fa fa-calendar"></i>
+                    </i>
+                    Calendar
+                  </a>
+                </li>
+              <?php } ?>
+
+                <li class="nav-item">
+                  <a class="nav-link" href="/gallery">
+                    <i class="fa fa-picture-o">
+                    </i>
+                    Gallery
+                  </a>
+                </li>
+
               </ul>
+              <form class="form-inline my-2 my-lg-0">
+                <ul class="navbar-nav ml-auto">
+                  <!-- Authentication Links -->
+                  @guest
+                  <li>
+                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}
+                    </a>
+                  </li>
+                  <li>
+                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}
+                    </a>
+                  </li>
+                  @else
+                  <li class="nav-item">
+                    <a class="nav-link" href="/schedule">
+                      <i class="fa fa-external-link-square"></i>
+                      </i>
+                      Place Order
+                    </a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                      <i class="fa fa-user-o"></i>
+                      </i>
+
+                      {{ Auth::user()->firstname." ".Auth::user()->lastname }}
+                      <span class="caret">
+                      </span>
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <?php $uid = Auth::user()->id; ?>
+                      <a href="/ordersummary" class="dropdown-item">View Orders</a>
+
+                      <a href="{{action('UserController@singleEdit', $uid)}}" class="dropdown-item">Update Account
+                      </a>
+                  <?php if ($id > 0) {
+                ?>
+                    <a href="/admin" class="dropdown-item">Admin</a>
+          <?php
+            } ?>
+                      <a class="dropdown-item" href="{{ route('logout') }}"
+                         onclick="event.preventDefault();
+                                  document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                      </a>
+                      <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                      </form>
+                    </div>
+                  </li>
+                  @endguest
+                </ul>
+              </form>
             </div>
           </div>
         </nav>
+
         <main class="py-4">
           @yield('content')
         </main>
