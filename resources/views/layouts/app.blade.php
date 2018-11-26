ca9a4b678f4e919956fb33e7efa75c38fc730710
Elijah <ejoebanks@gmail.com>
2018-11-26 13:48:45 -0600

    Removed unnecessary home link on navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,20 +36,15 @@
       <div id="app">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
           <div class="container">
-            <a class="navbar-brand" href="/">IHBO</a>
+            <a class="navbar-brand" href="/">
+              <i class="fas fa-horse fa-lg"> </i>
+              IHBO</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav mr-auto">
-                <li class="nav-item home">
-                  <a class="nav-link" href="/">
-                    <i class="fas fa-home fa-lg"></i>
-                    Home
-                    <span class="sr-only"></span>
-                    </a>
-                </li>
                 <?php
           if (isset($id) && $id > 0) {
               ?>
